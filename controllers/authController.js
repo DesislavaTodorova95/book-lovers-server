f6bd8d149497bca698c4ee37b806df5b42bac765
Desislava Todorova <desi_14_94@abv.bg>
2021-12-26 23:04:02 +0200

    formatted

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcrypt");
 const { createUser, getUserByEmail } = require("../services/userServices");
 router.post("/login", async (req, res) => {
   try {
-  
     const { email, password } = req.body;
     if (!(email && password)) {
       res.status(400).json("All inputs are required");
@@ -17,11 +16,6 @@ router.post("/login", async (req, res) => {
       throw new Error("This must be a valid email");
     }
     const user = await getUserByEmail(email);
-   
-    // if(!user){
-    //   res.status(200).json('No such user');
-    //   throw new Error('No such user')
-    // }
 
     if (user && (await bcrypt.compare(password, user.hashedPassword))) {
       const token = jwt.sign(
@@ -32,14 +26,13 @@ router.post("/login", async (req, res) => {
         }
       );
       user.token = token;
-      req.user = ({email: user.email, token}) // changed
-      
+      req.user = { email: user.email, token }; // changed
+
       res.json({ userId: user._id, email: user.email, token });
-    }else {
-      res.status(400).json('Wrong Email of Password');
-      throw new Error('Wrong Email of Password')
+    } else {
+      res.status(400).json("Wrong Email of Password");
+      throw new Error("Wrong Email of Password");
     }
-   
   } catch (err) {
     res.status(400).json(err);
   }
@@ -61,7 +54,7 @@ router.post("/register", async (req, res) => {
       res.status(400).json("This must be a valid email");
       throw new Error("This must be a valid email");
     }
-    if(password.length <4){
+    if (password.length < 4) {
       res.status(400).json("Passwords must be at least 4 characters long");
       throw new Error("Passwords must be at least 4 characters long");
     }
@@ -77,13 +70,10 @@ router.post("/register", async (req, res) => {
       throw new Error("User Already Exist. Please Login");
     }
 
-    //Encrypt user password
     hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create user in our database
     const user = await createUser(email, hashedPassword);
 
-    // Create token
     const token = jwt.sign(
       { user_id: user._id, email },
       process.env.JWT_SECRET,
@@ -91,15 +81,15 @@ router.post("/register", async (req, res) => {
         expiresIn: process.env.JWT_LIFETIME,
       }
     );
-    // save user token
+
     user.token = token;
-    req.user = ({email: user.email, token})
-    // return new user
+    req.user = { email: user.email, token };
+
     res.json({ userId: user._id, email: user.email, token });
   } catch (err) {
     res.status(400).json(err);
   }
-  // Our register logic ends here
+  
 });
 
 module.exports = router;
